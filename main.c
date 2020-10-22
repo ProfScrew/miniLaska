cc0b8c3f4173074c5ded645fe2be90758107831d
ProfScrew <maksym.nov@gmail.com>
2020-10-22 17:25:35 +0200

    Board construct

--- main.c
+++ main.c
@@ -1,8 +1,45 @@
 #include "stdio.h"
 
+void initBoard(char Board[7][7]){
 
+  int i,j;
+  for(i=0; i<7; i++){
+    for(j=0; j<7; j++){
+      if(i%2 == 0){
+        if(j%2 == 0)
+          Board [i] [j] = ' ';
+        else
+          Board [i] [j] = '#';
+
+      }
+      else{
+        if(j%2 == 0)
+          Board [i] [j] = '#';
+        else
+          Board [i] [j] = ' ';
+      }
+    }
+  }
+}
+
+
+
+void showBoard(char Board[7][7]){
+  int i,j;
+  for(i=0; i<7; i++){
+    for(j=0; j<7; j++){
+      printf("%c",Board [i] [j]);
+    }
+    printf("\n");
+  }
+
+}
 
 int main(int argc, char const *argv[]) {
-  printf("Hello world");
+  char Board [7] [7];
+  initBoard(Board);
+  showBoard(Board);
+  printf("\nHello world");
+
   return 0;
 }
