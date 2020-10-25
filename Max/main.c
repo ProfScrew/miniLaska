a09aed497252d64695cf27cde646f9456ec05e80
ProfScrew <maksym.nov@gmail.com>
2020-10-25 19:27:42 +0100

    Update 25 octobre

--- Max/main.c
+++ Max/main.c
@@ -1,5 +1,5 @@
-#include "stdio.h"
-
+#include <stdio.h>
+#include <stdlib.h>
 void initBoard(char Board[7][7][3]){
 
   int i,j;
@@ -39,7 +39,7 @@ void startPlayerOne(char Board[7][7][3]){
 }
 
 void startPlayerTwo(char Board[7][7][3]){
-  int i,j;
+  int i, j;
 
   for(i=4; i<7; i++){
     for(j=0; j<7; j++){
@@ -53,6 +53,46 @@ void startPlayerTwo(char Board[7][7][3]){
     }
   }
 }
+
+void checkWhiteMove(char Board[7][7][3]){
+  int i, j;
+
+
+  for(i=0; i<7; i++){
+    for(j=0; j<7; j++){
+      if(Board[i][j][0] == 'w'){
+        if(j==0){ /*white pieces on left side*/
+          if(i)
+          if(Board[i+1][j+1][0]== 'w' || Board[i+1][j+1][0] == 'W'){
+            /*cant move*/
+          }else if(Board[i+1][j+1][0] == ' '){
+            /*blank space can move here */
+          }else if(Board[i+1][j+1][0] == 'b' || Board[i+1][j+1][0] == 'B' ){
+            if(Board[][][])
+            if(Board[i+2][i+2][0] ==  ' '){
+              if(Board[][][] == )
+              /*can eat enemy piece */
+
+            }
+          }
+        }else if(j==6){
+          if (/* condition */) {
+            /* code */
+          }
+        }else{
+
+        }
+      }
+      if(Board[i][j][0] == 'W'){
+
+      }
+    }
+  }
+
+}
+
+
+
 
 
 
@@ -73,21 +113,48 @@ void showBoard(char Board[7][7][3]){
 int main(int argc, char const *argv[]) {
   char Board [7] [7] [3];
   char winner = 'n';
+  char turn = 'w';
   initBoard(Board);
   showBoard(Board);
   startPlayerOne(Board);
-  showBoard(Board);
   startPlayerTwo(Board);
   showBoard(Board);
+
+  /*system("clear");*/
 
   /* while loop where each player has a to do a move */
   while(winner == 'n'){
     /*so we have two funcition that move  but before we need to make a funcition
       at the ending where the system checks if someone has won*/
+      if(turn == 'w'){
+        /*white turn functions*/
+        printf("White move\n");
+        /*check moves tha white can do and write them*/
 
+        /*decide what moves he does*/
+        turn = 'b';
+      }
+      else{
+        /*black turn*/
+        printf("Black turn\n");
+        /*check moves tha white can do and write them*/
+
+        /*decide what moves he does*/
+
+        turn = 'b';
+      }
+
+      /*check if anyone has won the game*/
+
+      winner = 'b';
   }
 
-  printf("\nHello world\n");
+  if(winner =='w')
+    printf("White winner\n");
+  else
+    printf("Black winner\n");
+
+  printf("\nEnd.\n");
 
   return 0;
 }
