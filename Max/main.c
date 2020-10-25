9ba18b911bcb71f3d3485f389aedfad17b72a2f5
ProfScrew <maksym.nov@gmail.com>
2020-10-26 00:52:40 +0100

    Update 26 octobre 00:52

--- Max/main.c
+++ Max/main.c
@@ -53,31 +53,110 @@ void startPlayerTwo(char Board[7][7][3]){
     }
   }
 }
+void whiteSoldierMovement(){
+
+}
 
 void checkWhiteMove(char Board[7][7][3]){
   int i, j;
-
+  int k, l;
 
   for(i=0; i<7; i++){
     for(j=0; j<7; j++){
       if(Board[i][j][0] == 'w'){
-        if(j==0){ /*white pieces on left side*/
-          if(i)
-          if(Board[i+1][j+1][0]== 'w' || Board[i+1][j+1][0] == 'W'){
-            /*cant move*/
+        if(Board[i+1][j-1][0] == ' ' &&  (((i+1) > 0 && (i+1) < 7) && ((j-1) > 0 && (j-1) < 7 ))){
+          /*can move bot left*/
+        }else if(Board[i+1][j+1][0]== ' ' &&  (((i+1) > 0 && (i+1) < 7) && ((j+1) > 0 && (j+1) < 7 ))){
+          /*can move bot right*/
+        }
+        /*remember to put else */
+        else{
+          for(k=0;k<3;k++){
+            l = 1; /* thi variable is used to check if there are any kill moves if no then leave loop*/
+            if(((Board[i+1+k][j-1-k][0] == 'b')|| (Board[i+1+k][j-1-k][0] == 'B')) &&  (((i+1+k) > 0 && (i+1+k) < 7) && ((j-1-k) > 0 && (j-1-k) < 7 ))){  /*check left move */
+              if(Board[i+2+k][j-2-k][0]== ' ' &&  (((i+2+k) > 0 && (i+2+k) < 7) && ((j-2-k) > 0 && (j-2-k) < 7 ))){
+                l = 0;
+                /*can eat left at least once
+                  then we have to run the for loop if we can go further */
+
+                if(k!=0){}/*register more kill moves */
+
+                /*normal kill move */
+
+              }
+
+            }
+              if(((Board[i+1+k][j+1+k][0] == 'b')|| (Board[i+1+k][j+1+k][0] == 'B')) &&  (((i+1+k) > 0 && (i+1+k) < 7) && ((j+1+k) > 0 && (j+1+k) < 7 ))){  /*check right move*/
+                if(Board[i+2+k][j+2+k][0]== ' ' &&  (((i+2+k) > 0 && (i+2+k) < 7) && ((j+2+k) > 0 && (j+2+k) < 7 ))){
+                  l = 0;
+                  /*can eat right at least once
+                    then we have to run the for loop to check if we can go further*/
+
+                }
+              }
+
+            if(l == 1){
+              k=3; /*if cant find moves in the loop then stop*/
+              l=0;
+              }
+
+
+          }
+        }
+      }
+      if(Board[i][j][0] == 'W'){
+        /*first part normal moves to blank spot */
+        if(Board[i+1][j-1][0] == ' ' &&  (((i+1) > 0 && (i+1) < 7) && ((j-1) > 0 && (j-1) < 7 ))){        /*check bottom left*/
+
+        }else if(Board[i+1][j+1][0] == ' ' &&  (((i+1) > 0 && (i+1) < 7) && ((j+1) > 0 && (j+1) < 7 ))){  /*check bottom right*/
+
+        }else if(Board[i-1][j-1][0] == ' ' &&  (((i-1) > 0 && (i-1) < 7) && ((j-1) > 0 && (j-1) < 7 ))){  /*check top left*/
+
+        }else if(Board[i-1][j+1][0] == ' ' &&  (((i-1) > 0 && (i-1) < 7) && ((j+1) > 0 && (j+1) < 7 ))){  /*check top right*/
+
+        }else{
+          while(l==1){}
+        }
+
+      }
+    }
+  }
+
+
+
+/*  too complex if the other doesnt work come back to this
+  for(i=0; i<7; i++){
+    for(j=0; j<7; j++){
+      if(Board[i][j][0] == 'w'){
+        if(j==0){ /*white pieces on left side
+          if(i==5){
+            if(Board[i][j][0] == ' '){
+              /*can move for promotion
+            }
+          }else if(Board[i+1][j+1][0]== 'w' || Board[i+1][j+1][0] == 'W'){
+            /*cant move
           }else if(Board[i+1][j+1][0] == ' '){
-            /*blank space can move here */
+            /*blank space can move here
           }else if(Board[i+1][j+1][0] == 'b' || Board[i+1][j+1][0] == 'B' ){
-            if(Board[][][])
-            if(Board[i+2][i+2][0] ==  ' '){
-              if(Board[][][] == )
-              /*can eat enemy piece */
+            if(Board[i+2][j+2][0] ==  ' ' &&  (i+2) == 5){
+              /*just one move can eat
+            }else if(Board[i+2][i+2][0] == ' '){
+              if(left){
+
+              }else if(right){
+                if (right){
+                  ************
+                }else{/*can eat two peaces }
+              }
+            }
+
+              /*can eat enemy piece
 
             }
           }
         }else if(j==6){
-          if (/* condition */) {
-            /* code */
+          if (/* condition ) {
+            /* code
           }
         }else{
 
@@ -88,7 +167,7 @@ void checkWhiteMove(char Board[7][7][3]){
       }
     }
   }
-
+*/
 }
 
 
--- Max/secondmain.c
+++ Max/secondmain.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 int main(int argc, char const *argv[]) {
-  printf("Hello\n");
+  int x = 5;
+  if(x> 4 && x <10)
+    printf("ok\n");
+  else
+    printf("not ok\n");
   return 0;
 }
